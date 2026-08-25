ba21f2714436419746b7d3d59b615000344c71a2
davirpoliveira <davidodrigopiresdeoliveira@gmail.com>
2026-08-25 08:19:26 -0300

    Arrays Js

---
+++ array.js
@@ -0,0 +1,10 @@
+const frutas = ['banana', 'morango', 'maça', 'abacaxi'];
+
+console.log(frutas);
+console.log(frutas[0]);
+
+frutas.splice(5, 0, 'Péra');
+console.log(frutas)
+
+//interaçãp de dados
+
